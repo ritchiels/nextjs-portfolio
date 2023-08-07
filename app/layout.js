98c2dea7b1f2a78dab38595bd705ae1d2cfc7fbe
Ritchie Simmons <ritchiels.alt@gmail.com>
2023-08-06 23:54:17 -0400

    Prof aligned, Animation added

--- app/layout.js
+++ app/layout.js
@@ -9,8 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
  return (
     <html lang="en">
-      <body>{children}</body>
-      <Footer />
+      <body>
+        {children}
+        <Footer />
+    </body>
     </html>
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -9,11 +9,11 @@ const Home = () => {
             <Image
                 src={ProfilePic}
                 alt="Profile Image"
-                className="profile-image rounded-full"
+                className="profile-image rounded-full fade-in"
             />
             <div className="flex flex-col mb-10">
-                <h1 className="text-5xl font-semibold text-gray-800">Ritchie Simmons</h1>
-                <p className="text-sm font-comfortaa text-gray-600 mt-2">
+                <h1 className="text-5xl font-semibold text-lapiz-lazuli fade-in-header">Ritchie Simmons</h1>
+                <p className="text-sm font-semibold font-comfortaa text-gray-600 mt-2 fade-in-text">
                     Full-Stack web developer with a focus in React & MongoDB.
                 </p>
             </div>
