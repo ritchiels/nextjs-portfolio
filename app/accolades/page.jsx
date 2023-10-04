0c55531b47122ea5e193268e67399534ebf0dcde
Ritchie Simmons <ritchiels.alt@gmail.com>
2023-10-04 00:37:07 -0400

    accolades effect progress

--- app/accolades/page.jsx
+++ app/accolades/page.jsx
@@ -12,22 +12,22 @@ import Workspace from '../../img/workspace-addons.png'
 
 const Accolades = () => {
     return (
-        <div className="accolades-container flex justify-center">
+        <div className="accolades-container justify-center">
             <div className="card-group">
-                <div className="big-card card">
-                    
+                <div className="little-card">
+                    <Image src={Bootstrap} alt="Bootstrap" />
                 </div>
                 <div className="big-card card">
-                    
-                </div>
-                <div className="little-card card">
-                    
+                    <Image src={FrontEnd} alt="Front End Cert" />
                 </div>
                 <div className="little-card card">
 
                 </div>
                 <div className="little-card card">
 
+                </div>
+                <div className="big-card card">
+                    <Image src={FullStack} alt="Full Stack Cert" />
                 </div>
                 <div className="little-card card">
 
